Exslr/sing-box

Extend Snell protocol compatibility

option/snell.go
@@ -8,11 +8,12 @@ import (
 
 type _SnellInboundOptions struct {
 	ListenOptions
-	Version     int                    `json:"version"`
-	PSK         string                 `json:"psk"`
-	Users       []SnellUser            `json:"users,omitempty"`
-	ObfsOptions SnellObfsServerOptions `json:"-"`
-	V6Options   SnellV6Options         `json:"-"`
+	Version                 int                    `json:"version"`
+	PSK                     string                 `json:"psk,omitempty"`
+	Users                   []SnellUser            `json:"users,omitempty"`
+	MultiUserAuthentication string                 `json:"multi_user_authentication,omitempty"`
+	ObfsOptions             SnellObfsServerOptions `json:"-"`
+	V6Options               SnellV6Options         `json:"-"`
 }
 
 type SnellInboundOptions _SnellInboundOptions
@@ -33,7 +34,58 @@ func (o *SnellInboundOptions) UnmarshalJSON(content []byte) error {
 	default:
 		return E.New("snell: unsupported version: ", o.Version)
 	}
-	return badjson.UnmarshallExcluded(content, (*_SnellInboundOptions)(o), versionOptions)
+	err = badjson.UnmarshallExcluded(content, (*_SnellInboundOptions)(o), versionOptions)
+	if err != nil {
+		return err
+	}
+	var fields map[string]json.RawMessage
+	if err = json.Unmarshal(content, &fields); err != nil {
+		return err
+	}
+	_, pskConfigured := fields["psk"]
+	_, authenticationConfigured := fields["multi_user_authentication"]
+	if len(o.Users) == 0 {
+		if authenticationConfigured {
+			return E.New("snell: multi_user_authentication requires users")
+		}
+		if !pskConfigured || o.PSK == "" {
+			return E.New("snell: psk is required")
+		}
+		return nil
+	}
+	authentication := o.MultiUserAuthentication
+	if authentication == "" {
+		authentication = "userkey"
+	}
+	switch authentication {
+	case "userkey":
+		if !pskConfigured || o.PSK == "" {
+			return E.New("snell: psk is required with userkey multi-user authentication")
+		}
+		for index, user := range o.Users {
+			if user.pskConfigured {
+				return E.New("snell: users[", index, "].psk is not allowed with userkey authentication")
+			}
+			if !user.userKeyConfigured || user.UserKey == "" {
+				return E.New("snell: users[", index, "].userkey is required")
+			}
+		}
+	case "psk":
+		if pskConfigured {
+			return E.New("snell: top-level psk is not allowed with psk multi-user authentication")
+		}
+		for index, user := range o.Users {
+			if user.userKeyConfigured {
+				return E.New("snell: users[", index, "].userkey is not allowed with psk authentication")
+			}
+			if !user.pskConfigured || user.PSK == "" {
+				return E.New("snell: users[", index, "].psk is required")
+			}
+		}
+	default:
+		return E.New("snell: unknown multi_user_authentication: ", o.MultiUserAuthentication)
+	}
+	return nil
 }
 
 func (o SnellInboundOptions) MarshalJSON() ([]byte, error) {
@@ -72,12 +124,10 @@ func (o *SnellOutboundOptions) UnmarshalJSON(content []byte) error {
 	}
 	var versionOptions any
 	switch o.Version {
-	case 4:
+	case 0, 1, 2, 3, 4, 5:
 		versionOptions = &o.ObfsOptions
 	case 6:
 		versionOptions = &o.V6Options
-	case 0:
-		return E.New("snell: missing version")
 	default:
 		return E.New("snell: unsupported version: ", o.Version)
 	}
@@ -87,12 +137,10 @@ func (o *SnellOutboundOptions) UnmarshalJSON(content []byte) error {
 func (o SnellOutboundOptions) MarshalJSON() ([]byte, error) {
 	var versionOptions any
 	switch o.Version {
-	case 4:
+	case 0, 1, 2, 3, 4, 5:
 		versionOptions = o.ObfsOptions
 	case 6:
 		versionOptions = o.V6Options
-	case 0:
-		return nil, E.New("snell: missing version")
 	default:
 		return nil, E.New("snell: unsupported version: ", o.Version)
 	}
@@ -105,7 +153,37 @@ type SnellObfsServerOptions struct {
 
 type SnellUser struct {
 	Name    string `json:"name,omitempty"`
-	UserKey string `json:"userkey"`
+	UserKey string `json:"userkey,omitempty"`
+	PSK     string `json:"psk,omitempty"`
+
+	userKeyConfigured bool
+	pskConfigured     bool
+}
+
+func (u *SnellUser) UnmarshalJSON(content []byte) error {
+	type rawSnellUser struct {
+		Name    string  `json:"name,omitempty"`
+		UserKey *string `json:"userkey"`
+		PSK     *string `json:"psk"`
+	}
+	var raw rawSnellUser
+	if err := json.Unmarshal(content, &raw); err != nil {
+		return err
+	}
+	var fields map[string]json.RawMessage
+	if err := json.Unmarshal(content, &fields); err != nil {
+		return err
+	}
+	u.Name = raw.Name
+	_, u.userKeyConfigured = fields["userkey"]
+	_, u.pskConfigured = fields["psk"]
+	if raw.UserKey != nil {
+		u.UserKey = *raw.UserKey
+	}
+	if raw.PSK != nil {
+		u.PSK = *raw.PSK
+	}
+	return nil
 }
 
 type SnellObfsClientOptions struct {

protocol/snell/quic_nat.go
@@ -1,0 +1,348 @@
+package snell
+
+import (
+	"container/heap"
+	"context"
+	"io"
+	"net"
+	"net/netip"
+	"os"
+	"sync"
+	"time"
+
+	snellprotocol "github.com/sagernet/sing-snell"
+	"github.com/sagernet/sing/common/buf"
+	M "github.com/sagernet/sing/common/metadata"
+	N "github.com/sagernet/sing/common/network"
+	"github.com/sagernet/sing/common/pipe"
+)
+
+type quicProxyNATPrepareFunc func(source M.Socksaddr, destination M.Socksaddr, session *snellprotocol.QUICProxySession) (context.Context, N.PacketWriter)
+
+type quicProxyNATEntry struct {
+	key       netip.AddrPort
+	source    M.Socksaddr
+	session   *snellprotocol.QUICProxySession
+	conn      *quicProxyNATConn
+	expiresAt time.Time
+	index     int
+}
+
+type quicProxyNATHeap []*quicProxyNATEntry
+
+func (h quicProxyNATHeap) Len() int { return len(h) }
+func (h quicProxyNATHeap) Less(i, j int) bool {
+	return h[i].expiresAt.Before(h[j].expiresAt)
+}
+func (h quicProxyNATHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+	h[i].index = i
+	h[j].index = j
+}
+func (h *quicProxyNATHeap) Push(value any) {
+	entry := value.(*quicProxyNATEntry)
+	entry.index = len(*h)
+	*h = append(*h, entry)
+}
+func (h *quicProxyNATHeap) Pop() any {
+	old := *h
+	last := len(old) - 1
+	entry := old[last]
+	old[last] = nil
+	entry.index = -1
+	*h = old[:last]
+	return entry
+}
+
+type quicProxyNATService struct {
+	handler N.UDPConnectionHandlerEx
+	prepare quicProxyNATPrepareFunc
+	timeout time.Duration
+	access  sync.Mutex
+	entries map[netip.AddrPort]*quicProxyNATEntry
+	queue   quicProxyNATHeap
+	wake    chan struct{}
+	done    chan struct{}
+	closed  bool
+}
+
+func newQUICProxyNATService(handler N.UDPConnectionHandlerEx, prepare quicProxyNATPrepareFunc, timeout time.Duration) *quicProxyNATService {
+	service := &quicProxyNATService{
+		handler: handler,
+		prepare: prepare,
+		timeout: timeout,
+		entries: make(map[netip.AddrPort]*quicProxyNATEntry),
+		wake:    make(chan struct{}, 1),
+		done:    make(chan struct{}),
+	}
+	go service.run()
+	return service
+}
+
+func (s *quicProxyNATService) Session(source M.Socksaddr) (*quicProxyNATEntry, bool) {
+	key := source.AddrPort()
+	now := time.Now()
+	s.access.Lock()
+	entry, loaded := s.entries[key]
+	if loaded && !entry.expiresAt.After(now) {
+		s.removeLocked(entry)
+		loaded = false
+	}
+	s.access.Unlock()
+	if entry != nil && !loaded {
+		entry.conn.closeInternal()
+	}
+	if !loaded {
+		return nil, false
+	}
+	return entry, true
+}
+
+func (s *quicProxyNATService) NewPacket(entry *quicProxyNATEntry, payload []byte) bool {
+	now := time.Now()
+	s.access.Lock()
+	loaded := s.entries[entry.key] == entry
+	if loaded && !entry.expiresAt.After(now) {
+		s.removeLocked(entry)
+		loaded = false
+	} else if loaded {
+		entry.expiresAt = now.Add(s.timeout)
+		heap.Fix(&s.queue, entry.index)
+	}
+	s.access.Unlock()
+	if !loaded {
+		entry.conn.closeInternal()
+		return false
+	}
+	entry.conn.enqueue(payload, entry.session.Target())
+	return true
+}
+
+func (s *quicProxyNATService) NewSessionPacket(source M.Socksaddr, session *snellprotocol.QUICProxySession, payload []byte) bool {
+	key := source.AddrPort()
+	now := time.Now()
+	var expired *quicProxyNATEntry
+	s.access.Lock()
+	if s.closed {
+		s.access.Unlock()
+		return false
+	}
+	if existing, loaded := s.entries[key]; loaded {
+		if existing.expiresAt.After(now) {
+			s.access.Unlock()
+			return false
+		}
+		s.removeLocked(existing)
+		expired = existing
+	}
+	ctx, writer := s.prepare(source, session.Target(), session)
+	entry := &quicProxyNATEntry{
+		key:       key,
+		source:    source,
+		session:   session,
+		expiresAt: now.Add(s.timeout),
+	}
+	entry.conn = &quicProxyNATConn{
+		service:      s,
+		entry:        entry,
+		writer:       writer,
+		packetChan:   make(chan quicProxyNATPacket, 64),
+		done:         make(chan struct{}),
+		readDeadline: pipe.MakeDeadline(),
+	}
+	wake := len(s.queue) == 0
+	s.entries[key] = entry
+	heap.Push(&s.queue, entry)
+	entry.conn.enqueue(payload, session.Target())
+	go s.handler.NewPacketConnectionEx(ctx, entry.conn, source, session.Target(), func(error) {
+		s.remove(entry)
+	})
+	s.access.Unlock()
+	if expired != nil {
+		expired.conn.closeInternal()
+	}
+	if wake {
+		s.notify()
+	}
+	return true
+}
+
+func (s *quicProxyNATService) Len() int {
+	s.access.Lock()
+	defer s.access.Unlock()
+	return len(s.entries)
+}
+
+func (s *quicProxyNATService) Close() {
+	s.access.Lock()
+	if s.closed {
+		s.access.Unlock()
+		<-s.done
+		return
+	}
+	s.closed = true
+	entries := make([]*quicProxyNATEntry, 0, len(s.entries))
+	for _, entry := range s.entries {
+		entries = append(entries, entry)
+	}
+	clear(s.entries)
+	s.queue = nil
+	s.access.Unlock()
+	for _, entry := range entries {
+		entry.conn.closeInternal()
+	}
+	s.notify()
+	<-s.done
+}
+
+func (s *quicProxyNATService) remove(entry *quicProxyNATEntry) {
+	s.access.Lock()
+	if s.entries[entry.key] == entry {
+		s.removeLocked(entry)
+	}
+	s.access.Unlock()
+	entry.conn.closeInternal()
+}
+
+func (s *quicProxyNATService) removeLocked(entry *quicProxyNATEntry) {
+	delete(s.entries, entry.key)
+	if entry.index >= 0 {
+		heap.Remove(&s.queue, entry.index)
+	}
+}
+
+func (s *quicProxyNATService) run() {
+	defer close(s.done)
+	for {
+		s.access.Lock()
+		if s.closed {
+			s.access.Unlock()
+			return
+		}
+		now := time.Now()
+		var expired []*quicProxyNATEntry
+		for len(s.queue) > 0 && !s.queue[0].expiresAt.After(now) {
+			entry := heap.Pop(&s.queue).(*quicProxyNATEntry)
+			delete(s.entries, entry.key)
+			expired = append(expired, entry)
+		}
+		if len(s.queue) == 0 {
+			s.access.Unlock()
+			for _, entry := range expired {
+				entry.conn.closeInternal()
+			}
+			<-s.wake
+			continue
+		}
+		wait := time.Until(s.queue[0].expiresAt)
+		s.access.Unlock()
+		for _, entry := range expired {
+			entry.conn.closeInternal()
+		}
+		if wait < 0 {
+			wait = 0
+		}
+		timer := time.NewTimer(wait)
+		select {
+		case <-timer.C:
+		case <-s.wake:
+			if !timer.Stop() {
+				select {
+				case <-timer.C:
+				default:
+				}
+			}
+		}
+	}
+}
+
+func (s *quicProxyNATService) notify() {
+	select {
+	case s.wake <- struct{}{}:
+	default:
+	}
+}
+
+type quicProxyNATPacket struct {
+	buffer      *buf.Buffer
+	destination M.Socksaddr
+}
+
+type quicProxyNATConn struct {
+	service      *quicProxyNATService
+	entry        *quicProxyNATEntry
+	writer       N.PacketWriter
+	packetChan   chan quicProxyNATPacket
+	done         chan struct{}
+	access       sync.Mutex
+	closed       bool
+	readDeadline pipe.Deadline
+}
+
+func (c *quicProxyNATConn) enqueue(payload []byte, destination M.Socksaddr) {
+	buffer := buf.NewSize(len(payload))
+	_, _ = buffer.Write(payload)
+	c.access.Lock()
+	defer c.access.Unlock()
+	if c.closed {
+		buffer.Release()
+		return
+	}
+	select {
+	case c.packetChan <- quicProxyNATPacket{buffer: buffer, destination: destination}:
+	default:
+		buffer.Release()
+	}
+}
+
+func (c *quicProxyNATConn) ReadPacket(buffer *buf.Buffer) (M.Socksaddr, error) {
+	select {
+	case packet := <-c.packetChan:
+		_, err := buffer.ReadOnceFrom(packet.buffer)
+		packet.buffer.Release()
+		return packet.destination, err
+	case <-c.done:
+		return M.Socksaddr{}, io.ErrClosedPipe
+	case <-c.readDeadline.Wait():
+		return M.Socksaddr{}, os.ErrDeadlineExceeded
+	}
+}
+
+func (c *quicProxyNATConn) WritePacket(buffer *buf.Buffer, destination M.Socksaddr) error {
+	return c.writer.WritePacket(buffer, destination)
+}
+
+func (c *quicProxyNATConn) Close() error {
+	c.service.remove(c.entry)
+	return nil
+}
+
+func (c *quicProxyNATConn) closeInternal() {
+	c.access.Lock()
+	defer c.access.Unlock()
+	if c.closed {
+		return
+	}
+	c.closed = true
+	close(c.done)
+	for {
+		select {
+		case packet := <-c.packetChan:
+			packet.buffer.Release()
+		default:
+			return
+		}
+	}
+}
+
+func (c *quicProxyNATConn) LocalAddr() net.Addr { return c.entry.source.UDPAddr() }
+func (c *quicProxyNATConn) SetDeadline(time.Time) error {
+	return os.ErrInvalid
+}
+func (c *quicProxyNATConn) SetReadDeadline(deadline time.Time) error {
+	c.readDeadline.Set(deadline)
+	return nil
+}
+func (c *quicProxyNATConn) SetWriteDeadline(time.Time) error { return os.ErrInvalid }
+
+var _ N.PacketConn = (*quicProxyNATConn)(nil)

Newxin394/sing-box-ding

option/snell.go
@@ -18,8 +18,9 @@ type _SnellInboundOptions struct {
 
 type AbstractSnellInboundOptions struct {
 	ListenOptions
-	PSK   string      `json:"psk"`
-	Users []SnellUser `json:"users,omitempty"`
+	PSK                     string      `json:"psk,omitempty"`
+	Users                   []SnellUser `json:"users,omitempty"`
+	MultiUserAuthentication string      `json:"multi_user_authentication,omitempty" enum:"userkey,psk"`
 }
 
 type SnellInboundOptions _SnellInboundOptions
@@ -40,7 +41,58 @@ func (o *SnellInboundOptions) UnmarshalJSON(content []byte) error {
 	default:
 		return E.New("snell: unsupported version: ", o.Version)
 	}
-	return badjson.UnmarshallExcluded(content, (*_SnellInboundOptions)(o), versionOptions)
+	err = badjson.UnmarshallExcluded(content, (*_SnellInboundOptions)(o), versionOptions)
+	if err != nil {
+		return err
+	}
+	var fields map[string]json.RawMessage
+	if err = json.Unmarshal(content, &fields); err != nil {
+		return err
+	}
+	_, pskConfigured := fields["psk"]
+	_, authenticationConfigured := fields["multi_user_authentication"]
+	if len(o.Users) == 0 {
+		if authenticationConfigured {
+			return E.New("snell: multi_user_authentication requires users")
+		}
+		if !pskConfigured || o.PSK == "" {
+			return E.New("snell: psk is required")
+		}
+		return nil
+	}
+	authentication := o.MultiUserAuthentication
+	if authentication == "" {
+		authentication = "userkey"
+	}
+	switch authentication {
+	case "userkey":
+		if !pskConfigured || o.PSK == "" {
+			return E.New("snell: psk is required with userkey multi-user authentication")
+		}
+		for index, user := range o.Users {
+			if user.pskConfigured {
+				return E.New("snell: users[", index, "].psk is not allowed with userkey authentication")
+			}
+			if !user.userKeyConfigured || user.UserKey == "" {
+				return E.New("snell: users[", index, "].userkey is required")
+			}
+		}
+	case "psk":
+		if pskConfigured {
+			return E.New("snell: top-level psk is not allowed with psk multi-user authentication")
+		}
+		for index, user := range o.Users {
+			if user.userKeyConfigured {
+				return E.New("snell: users[", index, "].userkey is not allowed with psk authentication")
+			}
+			if !user.pskConfigured || user.PSK == "" {
+				return E.New("snell: users[", index, "].psk is required")
+			}
+		}
+	default:
+		return E.New("snell: unknown multi_user_authentication: ", o.MultiUserAuthentication)
+	}
+	return nil
 }
 
 func (o SnellInboundOptions) MarshalJSON() ([]byte, error) {
@@ -68,10 +120,10 @@ func (o SnellInboundOptions) DescribeSchema(builder schema.Builder) (*schema.Nod
 }
 
 type _SnellOutboundOptions struct {
-	Version int `json:"version" enum:"4,6"`
+	Version int `json:"version" enum:"1,2,3,4,5,6"`
 	AbstractSnellOutboundOptions
 	ObfsOptions SnellObfsClientOptions `json:"-"`
-	V6Options   SnellV6Options         `json:"-"`
+	V6Options   SnellV6OutboundOptions `json:"-"`
 }
 
 type AbstractSnellOutboundOptions struct {
@@ -92,7 +144,7 @@ func (o *SnellOutboundOptions) UnmarshalJSON(content []byte) error {
 	}
 	var versionOptions any
 	switch o.Version {
-	case 4:
+	case 1, 2, 3, 4, 5:
 		versionOptions = &o.ObfsOptions
 	case 6:
 		versionOptions = &o.V6Options
@@ -107,7 +159,7 @@ func (o *SnellOutboundOptions) UnmarshalJSON(content []byte) error {
 func (o SnellOutboundOptions) MarshalJSON() ([]byte, error) {
 	var versionOptions any
 	switch o.Version {
-	case 4:
+	case 1, 2, 3, 4, 5:
 		versionOptions = o.ObfsOptions
 	case 6:
 		versionOptions = o.V6Options
@@ -120,21 +172,77 @@ func (o SnellOutboundOptions) MarshalJSON() ([]byte, error) {
 }
 
 func (o SnellOutboundOptions) DescribeSchema(builder schema.Builder) (*schema.Node, error) {
-	return schema.DiscriminatedUnion(builder, "version", true, []schema.UnionVariant{
-		{Value: 4, StructType: reflect.TypeFor[SnellObfsClientOptions]()},
-		{Value: 6, StructType: reflect.TypeFor[SnellV6Options]()},
-	}, func(variant *schema.Node) error {
+	buildBase := func(variant *schema.Node) error {
 		return builder.FlattenStruct(variant, reflect.TypeFor[AbstractSnellOutboundOptions]())
-	})
+	}
+	union, err := schema.DiscriminatedUnion(builder, "version", true, []schema.UnionVariant{
+		{Value: 1, StructType: reflect.TypeFor[SnellObfsClientOptions]()},
+		{Value: 2, StructType: reflect.TypeFor[SnellObfsClientOptions]()},
+		{Value: 3, StructType: reflect.TypeFor[SnellObfsClientOptions]()},
+		{Value: 4, StructType: reflect.TypeFor[SnellObfsClientModernOptions]()},
+		{Value: 5, StructType: reflect.TypeFor[SnellObfsClientModernOptions]()},
+		{Value: 6, StructType: reflect.TypeFor[SnellV6OutboundOptions]()},
+	}, buildBase)
+	if err != nil {
+		return nil, err
+	}
+	return union, nil
 }
 
 type SnellObfsServerOptions struct {
-	ObfsMode string `json:"obfs_mode,omitempty" enum:"none,http,tls"`
+	ObfsMode string `json:"obfs_mode,omitempty" enum:"none,http"`
 }
 
 type SnellUser struct {
 	Name    string `json:"name,omitempty"`
-	UserKey string `json:"userkey"`
+	UserKey string `json:"userkey,omitempty"`
+	PSK     string `json:"psk,omitempty"`
+
+	userKeyConfigured bool
+	pskConfigured     bool
+}
+
+type snellUserSchema struct {
+	Name    string `json:"name,omitempty"`
+	UserKey string `json:"userkey,omitempty"`
+	PSK     string `json:"psk,omitempty"`
+}
+
+func (u SnellUser) DescribeSchema(builder schema.Builder) (*schema.Node, error) {
+	return builder.Define("SnellUser", func() (*schema.Node, error) {
+		node := schema.StrictObject()
+		err := builder.FlattenStruct(node, reflect.TypeFor[snellUserSchema]())
+		if err != nil {
+			return nil, err
+		}
+		return node, nil
+	})
+}
+
+func (u *SnellUser) UnmarshalJSON(content []byte) error {
+	type rawSnellUser struct {
+		Name    string  `json:"name,omitempty"`
+		UserKey *string `json:"userkey"`
+		PSK     *string `json:"psk"`
+	}
+	var raw rawSnellUser
+	if err := json.Unmarshal(content, &raw); err != nil {
+		return err
+	}
+	var fields map[string]json.RawMessage
+	if err := json.Unmarshal(content, &fields); err != nil {
+		return err
+	}
+	u.Name = raw.Name
+	_, u.userKeyConfigured = fields["userkey"]
+	_, u.pskConfigured = fields["psk"]
+	if raw.UserKey != nil {
+		u.UserKey = *raw.UserKey
+	}
+	if raw.PSK != nil {
+		u.PSK = *raw.PSK
+	}
+	return nil
 }
 
 type SnellObfsClientOptions struct {
@@ -142,6 +250,16 @@ type SnellObfsClientOptions struct {
 	ObfsHost string `json:"obfs_host,omitempty"`
 }
 
+type SnellObfsClientModernOptions struct {
+	ObfsMode string `json:"obfs_mode,omitempty" enum:"none,http"`
+	ObfsHost string `json:"obfs_host,omitempty"`
+}
+
 type SnellV6Options struct {
 	Mode string `json:"mode,omitempty" enum:"default,unshaped,unsafe-raw"`
+}
+
+type SnellV6OutboundOptions struct {
+	Mode          string `json:"mode,omitempty" enum:"default,unshaped,unsafe-raw"`
+	QUICProxyMode bool   `json:"quic_proxy_mode,omitempty"`
 }

protocol/snell/quic_nat.go
@@ -1,0 +1,380 @@
+package snell
+
+import (
+	"container/heap"
+	"context"
+	"io"
+	"net"
+	"net/netip"
+	"os"
+	"sync"
+	"time"
+
+	snellprotocol "github.com/sagernet/sing-snell"
+	"github.com/sagernet/sing/common/buf"
+	M "github.com/sagernet/sing/common/metadata"
+	N "github.com/sagernet/sing/common/network"
+	"github.com/sagernet/sing/common/pipe"
+)
+
+type quicProxyNATPrepareFunc func(source M.Socksaddr, destination M.Socksaddr, session *snellprotocol.QUICProxySession) (context.Context, N.PacketWriter)
+
+type quicProxyNATEntry struct {
+	key       netip.AddrPort
+	source    M.Socksaddr
+	session   *snellprotocol.QUICProxySession
+	conn      *quicProxyNATConn
+	expiresAt time.Time
+	index     int
+}
+
+func (e *quicProxyNATEntry) close() {
+	e.conn.closeInternal()
+}
+
+type quicProxyNATHeap []*quicProxyNATEntry
+
+func (h quicProxyNATHeap) Len() int { return len(h) }
+func (h quicProxyNATHeap) Less(i, j int) bool {
+	return h[i].expiresAt.Before(h[j].expiresAt)
+}
+
+func (h quicProxyNATHeap) Swap(i, j int) {
+	h[i], h[j] = h[j], h[i]
+	h[i].index = i
+	h[j].index = j
+}
+
+func (h *quicProxyNATHeap) Push(value any) {
+	entry := value.(*quicProxyNATEntry)
+	entry.index = len(*h)
+	*h = append(*h, entry)
+}
+
+func (h *quicProxyNATHeap) Pop() any {
+	old := *h
+	last := len(old) - 1
+	entry := old[last]
+	old[last] = nil
+	entry.index = -1
+	*h = old[:last]
+	return entry
+}
+
+type quicProxyNATService struct {
+	handler N.UDPConnectionHandlerEx
+	prepare quicProxyNATPrepareFunc
+	timeout time.Duration
+	access  sync.Mutex
+	entries map[netip.AddrPort]*quicProxyNATEntry
+	queue   quicProxyNATHeap
+	wake    chan struct{}
+	done    chan struct{}
+	closed  bool
+}
+
+func newQUICProxyNATService(handler N.UDPConnectionHandlerEx, prepare quicProxyNATPrepareFunc, timeout time.Duration) *quicProxyNATService {
+	service := &quicProxyNATService{
+		handler: handler,
+		prepare: prepare,
+		timeout: timeout,
+		entries: make(map[netip.AddrPort]*quicProxyNATEntry),
+		wake:    make(chan struct{}, 1),
+		done:    make(chan struct{}),
+	}
+	go service.run()
+	return service
+}
+
+func (s *quicProxyNATService) Session(source M.Socksaddr) (*quicProxyNATEntry, bool) {
+	key := source.AddrPort()
+	now := time.Now()
+	s.access.Lock()
+	entry, loaded := s.entries[key]
+	if loaded && !entry.expiresAt.After(now) {
+		s.removeLocked(entry)
+		loaded = false
+	}
+	s.access.Unlock()
+	if entry != nil && !loaded {
+		entry.close()
+	}
+	if !loaded {
+		return nil, false
+	}
+	return entry, true
+}
+
+func (s *quicProxyNATService) NewPacket(entry *quicProxyNATEntry, payload []byte) bool {
+	now := time.Now()
+	s.access.Lock()
+	loaded := s.entries[entry.key] == entry
+	if loaded && !entry.expiresAt.After(now) {
+		s.removeLocked(entry)
+		loaded = false
+	} else if loaded {
+		entry.expiresAt = now.Add(s.timeout)
+		heap.Fix(&s.queue, entry.index)
+		if len(payload) > 0 {
+			entry.conn.enqueue(payload, entry.session.Target())
+		}
+	}
+	s.access.Unlock()
+	if !loaded {
+		entry.close()
+		return false
+	}
+	return true
+}
+
+func (s *quicProxyNATService) NewSession(source M.Socksaddr, session *snellprotocol.QUICProxySession, payload []byte) bool {
+	key := source.AddrPort()
+	now := time.Now()
+	var expired *quicProxyNATEntry
+	s.access.Lock()
+	if s.closed {
+		s.access.Unlock()
+		return false
+	}
+	if existing, loaded := s.entries[key]; loaded {
+		if existing.expiresAt.After(now) {
+			s.access.Unlock()
+			return false
+		}
+		s.removeLocked(existing)
+		expired = existing
+	}
+	entry := &quicProxyNATEntry{
+		key:       key,
+		source:    source,
+		session:   session,
+		expiresAt: now.Add(s.timeout),
+	}
+	ctx, conn := s.newConnection(entry)
+	entry.conn = conn
+	conn.enqueue(payload, session.Target())
+	wake := len(s.queue) == 0
+	s.entries[key] = entry
+	heap.Push(&s.queue, entry)
+	s.access.Unlock()
+	if expired != nil {
+		expired.close()
+	}
+	if wake {
+		s.notify()
+	}
+	go s.handleConnection(ctx, entry, conn)
+	return true
+}
+
+// The caller must hold s.access.
+func (s *quicProxyNATService) newConnection(entry *quicProxyNATEntry) (context.Context, *quicProxyNATConn) {
+	ctx, writer := s.prepare(entry.source, entry.session.Target(), entry.session)
+	return ctx, &quicProxyNATConn{
+		service:      s,
+		entry:        entry,
+		writer:       writer,
+		packetChan:   make(chan quicProxyNATPacket, 64),
+		done:         make(chan struct{}),
+		readDeadline: pipe.MakeDeadline(),
+	}
+}
+
+func (s *quicProxyNATService) handleConnection(ctx context.Context, entry *quicProxyNATEntry, conn *quicProxyNATConn) {
+	s.handler.NewPacketConnectionEx(ctx, conn, entry.source, entry.session.Target(), func(error) {
+		s.remove(entry)
+	})
+}
+
+func (s *quicProxyNATService) Len() int {
+	s.access.Lock()
+	defer s.access.Unlock()
+	return len(s.entries)
+}
+
+func (s *quicProxyNATService) Close() {
+	s.access.Lock()
+	if s.closed {
+		s.access.Unlock()
+		<-s.done
+		return
+	}
+	s.closed = true
+	entries := make([]*quicProxyNATEntry, 0, len(s.entries))
+	for _, entry := range s.entries {
+		entries = append(entries, entry)
+	}
+	clear(s.entries)
+	s.queue = nil
+	s.access.Unlock()
+	for _, entry := range entries {
+		entry.close()
+	}
+	s.notify()
+	<-s.done
+}
+
+func (s *quicProxyNATService) remove(entry *quicProxyNATEntry) {
+	s.access.Lock()
+	if s.entries[entry.key] == entry {
+		s.removeLocked(entry)
+	}
+	s.access.Unlock()
+	entry.close()
+}
+
+func (s *quicProxyNATService) removeLocked(entry *quicProxyNATEntry) {
+	delete(s.entries, entry.key)
+	if entry.index >= 0 {
+		heap.Remove(&s.queue, entry.index)
+	}
+}
+
+func (s *quicProxyNATService) run() {
+	defer close(s.done)
+	for {
+		s.access.Lock()
+		if s.closed {
+			s.access.Unlock()
+			return
+		}
+		now := time.Now()
+		var expired []*quicProxyNATEntry
+		for len(s.queue) > 0 && !s.queue[0].expiresAt.After(now) {
+			entry := heap.Pop(&s.queue).(*quicProxyNATEntry)
+			delete(s.entries, entry.key)
+			expired = append(expired, entry)
+		}
+		if len(s.queue) == 0 {
+			s.access.Unlock()
+			for _, entry := range expired {
+				entry.close()
+			}
+			<-s.wake
+			continue
+		}
+		wait := time.Until(s.queue[0].expiresAt)
+		s.access.Unlock()
+		for _, entry := range expired {
+			entry.close()
+		}
+		if wait < 0 {
+			wait = 0
+		}
+		timer := time.NewTimer(wait)
+		select {
+		case <-timer.C:
+		case <-s.wake:
+			if !timer.Stop() {
+				select {
+				case <-timer.C:
+				default:
+				}
+			}
+		}
+	}
+}
+
+func (s *quicProxyNATService) notify() {
+	select {
+	case s.wake <- struct{}{}:
+	default:
+	}
+}
+
+type quicProxyNATPacket struct {
+	buffer      *buf.Buffer
+	destination M.Socksaddr
+}
+
+type quicProxyNATConn struct {
+	service      *quicProxyNATService
+	entry        *quicProxyNATEntry
+	writer       N.PacketWriter
+	packetChan   chan quicProxyNATPacket
+	done         chan struct{}
+	access       sync.Mutex
+	writeAccess  sync.RWMutex
+	closed       bool
+	readDeadline pipe.Deadline
+}
+
+func (c *quicProxyNATConn) enqueue(payload []byte, destination M.Socksaddr) {
+	buffer := buf.NewSize(len(payload))
+	_, _ = buffer.Write(payload)
+	c.access.Lock()
+	defer c.access.Unlock()
+	if c.closed {
+		buffer.Release()
+		return
+	}
+	select {
+	case c.packetChan <- quicProxyNATPacket{buffer: buffer, destination: destination}:
+	default:
+		buffer.Release()
+	}
+}
+
+func (c *quicProxyNATConn) ReadPacket(buffer *buf.Buffer) (M.Socksaddr, error) {
+	select {
+	case packet := <-c.packetChan:
+		_, err := buffer.ReadOnceFrom(packet.buffer)
+		packet.buffer.Release()
+		return packet.destination, err
+	case <-c.done:
+		return M.Socksaddr{}, io.ErrClosedPipe
+	case <-c.readDeadline.Wait():
+		return M.Socksaddr{}, os.ErrDeadlineExceeded
+	}
+}
+
+func (c *quicProxyNATConn) WritePacket(buffer *buf.Buffer, destination M.Socksaddr) error {
+	c.writeAccess.RLock()
+	defer c.writeAccess.RUnlock()
+	c.access.Lock()
+	if c.closed {
+		c.access.Unlock()
+		buffer.Release()
+		return net.ErrClosed
+	}
+	c.access.Unlock()
+	return c.writer.WritePacket(buffer, destination)
+}
+
+func (c *quicProxyNATConn) Close() error {
+	c.service.remove(c.entry)
+	return nil
+}
+
+func (c *quicProxyNATConn) closeInternal() {
+	c.writeAccess.Lock()
+	defer c.writeAccess.Unlock()
+	c.access.Lock()
+	defer c.access.Unlock()
+	if c.closed {
+		return
+	}
+	c.closed = true
+	close(c.done)
+	for {
+		select {
+		case packet := <-c.packetChan:
+			packet.buffer.Release()
+		default:
+			return
+		}
+	}
+}
+
+func (c *quicProxyNATConn) LocalAddr() net.Addr { return c.entry.source.UDPAddr() }
+func (c *quicProxyNATConn) SetDeadline(time.Time) error {
+	return os.ErrInvalid
+}
+
+func (c *quicProxyNATConn) SetReadDeadline(deadline time.Time) error {
+	c.readDeadline.Set(deadline)
+	return nil
+}
+func (c *quicProxyNATConn) SetWriteDeadline(time.Time) error { return os.ErrInvalid }
+
+var _ N.PacketConn = (*quicProxyNATConn)(nil)