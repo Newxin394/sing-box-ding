lingqiqi5211/sing-box-p

DNS: Fix some cache issues
- Fix use-after-free in DNS cache and RDRC persistence.
  buf.Get/Put (backed by sync.Pool) were used inside bbolt Batch
  closures. Since Batch may retry the callback and commits data
  asynchronously after the callback returns, the pooled buffers could
  be reclaimed and overwritten by other goroutines before the
  transaction was flushed to disk, causing widespread cache corruption.
  Move buf.Get before and buf.Put after the Batch call to ensure
  buffers remain valid for the entire transaction lifetime.

- Preserve in-memory cache entry when bbolt flush fails.
  Previously, entries were deleted from memory regardless of write
  success, resulting in data loss (neither on disk nor in memory).

- Log a warning when unpacking a persistent DNS cache entry fails,
  and add DeleteDNSCache to clean up corrupted entries on load rather
  than silently skipping them on every query.

dns/client.go
@@ -442,6 +442,10 @@ func (c *Client) loadPersistentResponse(question dns.Question, transport adapter
 	response := new(dns.Msg)
 	err := response.Unpack(rawMessage)
 	if err != nil {
+		if c.logger != nil {
+			c.logger.Warn("load persistent DNS cache for ", question.Name, ": unpack failed: ", err)
+		}
+		c.dnsCache.DeleteDNSCache(transport.Tag(), question.Name, question.Qtype)
 		return nil, 0, false
 	}
 	if c.disableExpire {

Newxin394/sing-box-ding

dns/client.go
@@ -594,6 +594,10 @@ func (c *Client) loadPersistentResponse(key dnsCacheKey) (*dns.Msg, int, bool) {
 	response := new(dns.Msg)
 	err := response.Unpack(rawMessage)
 	if err != nil {
+		if c.logger != nil {
+			c.logger.Warn("load persistent DNS cache for ", key.Name, ": unpack failed: ", err)
+		}
+		c.dnsCache.DeleteDNSCache(key.persistentName(), key.Name, key.Qtype, rawMessage)
 		return nil, 0, false
 	}
 	if c.disableExpire {